b050c1592f19f0d6a812a65506ef749ee2ee349c
Cameron Skidmore <cskidz99@Camerons-MacBook-Air.local>
2016-11-01 15:38:44 -0600

    Project complete (minus black diamonds)

--- js/lessonHider.js
+++ js/lessonHider.js
@@ -2,10 +2,26 @@ angular.module('directivePractice').directive('lessonHider',function(){
   return {
     restrict: 'E',
     templateUrl: 'lessonHider.html',
+    scope: {
+      lesson: '=',
+      dayAlert: '&',
+    },
+    controller: function($scope,lessonService){
+      $scope.getSchedule = lessonService.getSchedule();
+    },
     link: function(scope,element,attributes){
-      console.log(scope)
-      console.log(element)
-      console.log(attributes)
+      scope.getSchedule.then(function(response){
+        scope.schedule = response.data;
+
+        scope.schedule.forEach(function(scheduleDay){
+          if(scheduleDay.lesson === scope.lesson){
+            element.css('text-decoration', 'line-through');
+            scope.lessonDay = scheduleDay.weekday;
+            // console.log(scope.lessonDay);
+            return;
+          }
+        })
+      })
     }
   }
 })
